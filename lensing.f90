18c91889bd2953b16745c743517d9b7abe923869
antoine_marchal <antoine.marchal@obspm.fr>
2016-02-13 12:00:28 +0100

    save lensing project

--- lensing.f90
+++ lensing.f90
@@ -17,8 +17,10 @@ Program lensing
   double precision,allocatable::maginf(:), magsup(:)
   character(len=10),allocatable::fich_sortie(:)
 
-  double precision,dimension(:),allocatable:: RA,DEC,e1,e2,de1,de2,z,err_z,snr_win,mag_y
-  double precision,dimension(:),allocatable:: objid,z2,zErr,zPhoto,zPhotoErr,type,ra2,dec2 &
+  double precision,dimension(:),allocatable:: &
+  RA,DEC,e1,e2,de1,de2,z,err_z,snr_win,mag_y
+  double precision,dimension(:),allocatable:: &
+  objid,z2,zErr,zPhoto,zPhotoErr,type,ra2,dec2 &
        ,veldisp,ved,mabs_r
   
   double precision,parameter::rmin=0.00333,rmax=0.1667
@@ -31,7 +33,8 @@ Program lensing
   double precision::et=0., er=0.
   integer::k=0.
 
-  double precision,dimension(:),allocatable::shearT, shearR, N, r2
+  double precision,dimension(:),allocatable::
+  shearT, shearR, N, r2
   double precision::sigma 
   double precision::moy=0.
   double precision::var=0.
@@ -54,24 +57,28 @@ Program lensing
   Call set_cosmology(0.3d0,0.7d0,0.3d0,Ho)
   call init_random_seed(1)   
 
-  allocate( vdinf(dim1), vdsup(dim1), maginf(dim2), magsup(dim2), fich_sortie(dim1) ) 
+  allocate( vdinf(dim1), vdsup(dim1), maginf(dim2),
+  magsup(dim2), fich_sortie(dim1) ) 
 
 !------------------------------------------------------------
-  open(10,file='/home/amarchal/file_NGVS.txt',status='old',action='read',iostat=ios)
+  open(10,file='/home/amarchal/file_NGVS.txt',status='old', &
+       action='read',iostat=ios)
     if(ios .ne. 0)stop"Pb !"
     do i=1,116
        read(10,*)NGVS_maille(i)
     enddo
   close(10)
 
-  open(11,file='/home/amarchal/file_SDSS.txt',status='old',action='read',iostat=ios)
+  open(11,file='/home/amarchal/file_SDSS.txt',status='old', &
+       action='read',iostat=ios)
     if(ios .ne. 0)stop"Pb !"
     do i=1,116
        read(11,*)SDSS_maille(i)
     enddo
   close(11)
 
-  open(12,file='/home/amarchal/file_disp.txt',status='old',action='read',iostat=ios)
+  open(12,file='/home/amarchal/file_disp.txt',status='old', &
+       action='read',iostat=ios)
      if(ios .ne. 0)stop"Pb !"
      read(12,*)
      do i=1,dim1
@@ -79,7 +86,8 @@ Program lensing
      enddo
   close(12)
 
-  open(13,file='/home/amarchal/file_mag.txt',status='old',action='read',iostat=ios)
+  open(13,file='/home/amarchal/file_mag.txt',status='old', &
+       action='read',iostat=ios)
      if(ios .ne. 0)stop"Pb !"
      read(13,*)
      do i=1,dim2
@@ -87,7 +95,8 @@ Program lensing
      enddo
   close(13)
 
-  open(14,file='/home/amarchal/file_sortie.txt',status='old',action='read',iostat=ios)
+  open(14,file='/home/amarchal/file_sortie.txt',status='old' &
+       ,action='read',iostat=ios)
      if(ios .ne. 0)stop"Pb !"
      read(14,*)
      do i=1,dim1
@@ -107,10 +116,12 @@ Program lensing
         shearT(i)=0.; N(i)=0.; r2=0.; sumB(i)=0.; shearR(i)=0.
      enddo
 
-!-------------------------------------------------------------------------------------
+!-------------------------------------------------------------
      do l=1,116
-        call lecture_NGVS('/home/amarchal/NGVS_data/'//NGVS_maille(l),nlNGVS,RA,DEC,e1,e2,de1,de2,z,err_z,snr_win,mag_y)
-        call lecture_SDSS('/home/amarchal/new_SDSS_data/'//SDSS_maille(l),nlSDSS,objid,z2,zErr,zPhoto,zPhotoErr,type,ra2,dec2,veldisp,ved,mabs_r)
+        call lecture_NGVS('/home/amarchal/NGVS_data/'//NGVS_maille(l), &
+        nlNGVS,RA,DEC,e1,e2,de1,de2,z,err_z,snr_win,mag_y)
+        call lecture_SDSS('/home/amarchal/new_SDSS_data/'//SDSS_maille(l), &
+        nlSDSS,objid,z2,zErr,zPhoto,zPhotoErr,type,ra2,dec2,veldisp,ved,mabs_r)
 !-----------------------calcul de sigmaS----------------------
         do i=1,nlNGVS-1  
            if (e1(i) .ne. 0 .and. e2(i) .ne. 0 .and. e1(i) .gt. -1 &
@@ -130,16 +141,18 @@ Program lensing
 !------------------------------------------------------------
 !   wiso = 1. / sigma/sigma 
         do j=1,nlSDSS-1   
-!        if (z2(j) .gt. 0 .and. z2(j) .lt. 0.5 .and. veldisp(j) .gt. 0. .and. veldisp(j) .lt. 180. ) then 
+           !if (z2(j) .gt. 0 .and. z2(j) .lt. 0.5 .and. veldisp(j) .gt. 0. &
+           !.and. veldisp(j) .lt. 180. ) then 
            !if (z2(j) .gt. 0.05 .and. z2(j) .lt. 0.6 .and. veldisp(j) .gt. vdinf(m)  &
            !.and. veldisp(j) .lt. vdsup(m)) then 
             
-           if (z2(j) .gt. 0.05 .and. z2(j) .lt. 0.6 .and. mabs_r(j) .gt. magsup(m) &
-           .and. mabs_r(j) .lt. maginf(m) .and. veldisp(j) .gt. 10. .and. veldisp(j) .lt. 500.) then 
+           if (z2(j) .gt. 0.05 .and. z2(j) .lt. 0.6 .and. mabs_r(j) &
+                .gt. magsup(m) .and. mabs_r(j) .lt. maginf(m) .and. &
+                veldisp(j) .gt. 10. .and. veldisp(j) .lt. 500.) then 
 
               do i=1,nlNGVS
-                 if (z(i)+err_z(i) .gt. 0.7 .and. e1(i) .ne. 0 .and. e2(i) .ne. 0 .and. e1(i) .gt. -1 &
-                 .and. e1(i) .lt. 1) then 
+                 if (z(i)+err_z(i) .gt. 0.7 .and. e1(i) .ne. 0 .and. e2(i) &
+                      .ne. 0 .and. e1(i) .gt. -1 .and. e1(i) .lt. 1) then 
 
                     wiso = 1./( sigma*sigma + 0.5*(de1(i)*de1(i) + de2(i)*de2(i)))
 
@@ -169,10 +182,9 @@ Program lensing
                        N(k)=N(k)+1
                     
                        sumB(k)=sumB(k)+B2*wiso
-           
- ! write(14,*)x*60.,y*60.
                     endif
-                    x=0.; y=0.; r=0.; k=0.; et=0.; er=0.; alpha=0.; lr=0.; dl=0.; ds=0.; B=0.; B2=0; wiso=0.
+                    x=0.; y=0.; r=0.; k=0.; et=0.; er=0.; alpha=0.; &
+                    lr=0.; dl=0.; ds=0.; B=0.; B2=0; wiso=0.
                  endif
               enddo
               moydisp=moydisp+(veldisp(j)*veldisp(j)*(1/ved(j)**2.))
@@ -199,14 +211,16 @@ Program lensing
 !     ErrT(i)=0.26331349542305638/sqrt(N(i))!*sumB(i))   !A vérifier
         shearT(i)=shearT(i)/sumB(i)
         shearR(i)=shearR(i)/sumB(i)
-        write(*,'(30(x,F15.6))') r2(i)*60,shearT(i),shearR(i),1./sqrt(sumB(i)),N(i)
+        write(*,'(30(x,F15.6))') r2(i)*60,shearT(i),shearR(i), &
+             1./sqrt(sumB(i)),N(i)
      enddo
 !-----------------------Resultats----------------------
      open(14,file=fich_sortie(m),status='replace', action='write',iostat=ios)
      if(ios .ne. 0)stop"Pb !"
      write(14,*)"r(')    shearT   shearR   Err_ShearT    N"
      do i=1,dim
-           write(14,'(30(x,F15.6))') r2(i)*60,shearT(i),shearR(i),1./sqrt(sumB(i)),N(i)
+        write(14,'(30(x,F15.6))') r2(i)*60,shearT(i),shearR(i), &
+             1./sqrt(sumB(i)),N(i)
      enddo
      close(14) 
 !-------------------------------------------------------------
@@ -218,7 +232,8 @@ Program lensing
      mag=mag/Nlens
 !-------------------------------------------------------------
 !-------------Ecriture resultat vitesses de dispersions-------
-     open(15,file='test/resultat.txt',status='old',position='append', action='write',iostat=ios)
+     open(15,file='test/resultat.txt',status='old',position='append', &
+          action='write',iostat=ios)
      if(ios .ne. 0)stop"Pb !"
         write(15,'(30(x,F15.6))')moydisp,err_vstar,mag
      close(15) 
